9b7b01d54adbe486220d12804bdef82b26616407
vcjuliocesar <vcjuliocesar@outlook.com>
2023-10-30 13:35:55 -0600

    created error handler exception

--- src/app/main.py
+++ src/app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from src.infrastructure.exceptions.error_handler import ErrorHandler
 from src.domain.models.base_entity import init
 from src.app.routers.api.user import user_router
 
 app = FastAPI()
+
+app.add_middleware(ErrorHandler)
 
 app.include_router(user_router)
 
