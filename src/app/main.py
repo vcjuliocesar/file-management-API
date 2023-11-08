82a3cc1f3247a3801aeffc0a76c6d4cd9436a998
vcjuliocesar <vcjuliocesar@outlook.com>
2023-11-07 20:20:31 -0600

    add jinja2

--- src/app/main.py
+++ src/app/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException,Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from src.infrastructure.exceptions.error_handler import ErrorHandler
 from src.infrastructure.configs.enviroment import get_enviroment_settinngs
 from src.domain.models.base_entity import init
@@ -17,6 +19,17 @@ app.version = env().API_VERSION
 app.description = env().APP_DESCRIPTION
 
 app.add_middleware(ErrorHandler)
+
+templates = Jinja2Templates(directory="src/app/templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request:Request):
+    
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.exception_handler(HTTPException)
+async def not_found_exception_handler(request: Request, exc: HTTPException):
+    return templates.TemplateResponse("error.html", {"request": request}, status_code=404)
 
 app.include_router(auth_router)
 
