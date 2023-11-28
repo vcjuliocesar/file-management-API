e539e136d2b94eb2a9490eab0b823fa29d5b51ac
vcjuliocesar <vcjuliocesar@outlook.com>
2023-11-28 17:18:43 -0600

    apply dependency injection

--- src/app/usecases/file/delete_file_use_case.py
+++ src/app/usecases/file/delete_file_use_case.py
@@ -1,10 +1,11 @@
+from fastapi import Depends
 from src.services.file_service import FileService
 
 class DeleteFileUseCase:
     
-    def __init__(self) -> None:
+    def __init__(self,file_service:FileService = Depends()) -> None:
         
-        self.file_service = FileService()
+        self.file_service = file_service
         
     def execute(self,id:int,token) -> None:
         
--- src/app/usecases/file/find_file_by_id_use_case.py
+++ src/app/usecases/file/find_file_by_id_use_case.py
@@ -1,10 +1,11 @@
+from fastapi import Depends
 from src.services.file_service import FileService
 from src.domain.models.file_entity import FileEntity
 class FindFileByIdUseCase:
     
-    def __init__(self) -> None:
+    def __init__(self,file_service:FileService = Depends()) -> None:
         
-        self.file_service = FileService()
+        self.file_service = file_service
         
     def execute(self,file_id:int,token) -> FileEntity:
         
